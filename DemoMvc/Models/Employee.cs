bc66281a3a7fe33efcafad9411fb485248f1abd6
prakashjoe <daggu.prakash@capgemini.com>
2017-12-27 14:50:52 +0530

    Add project files.

---
+++ DemoMvc/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DemoMvc.Models
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string Name { get; set; }
+    }
+}
